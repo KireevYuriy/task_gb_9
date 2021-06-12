923c148c8d85383913879737b0e534ca3cd0ba51
Yuriy <kireev_juriyy@rambler.ru>
2021-06-13 00:17:17 +0800

    Done task_9_1

---
+++ task_9_1.py
@@ -0,0 +1,18 @@
+import time
+
+
+class TrafficLight:
+    __color = ("Red", "Yellow", "Green")
+
+    def running(self):
+        while True:
+            print(TrafficLight.__color[0])
+            time.sleep(7)
+            print(TrafficLight.__color[1])
+            time.sleep(2)
+            print(TrafficLight.__color[2])
+            time.sleep(5)
+
+
+a = TrafficLight()
+a.running()
